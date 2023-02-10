2a80fbb7d4f81eb55a6c2880b2475068f51c5f0a
tugi <blueqwinn@gmail.com>
2023-02-10 21:20:05 +0300

    first commit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+"lion,hippo,cheetah,giraffe"
 
+"lion,hippo,cheetah,giraffe".split(",")
 end
+
+def .to_a
+(1..10).to_a
+# => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def join 
+    ["a", "b" , "c" , "d"].join
+   # => "abcd"
+   
+   
+def join2
+   ["a", "b" , "c" , "d"].join(" :-) ")
+    # => "a :-) b :-) c :-) d :-)"
